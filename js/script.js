d827f7a46ce6f7ea703da4cb1ccdb046c8aac28d
Mateus Dias <mateusdi.santosz@gmail.com>
2024-08-15 13:57:08 -0300

    reestruturação do layout

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// script.js
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const content = document.querySelector('.content');
+//     const text = "Olá! Sou Mateus e este é o meu portfólio. Aqui você encontrará...Olá! Sou Mateus e este é o meu portfólio. Aqui você encontrará..Olá! Sou Mateus e este é o meu portfólio. Aqui você encontrará..Olá! Sou Mateus e este é o meu portfólio. Aqui você encontrará..tfólio. Aqui você encontrará..tfólio. Aqui você encontrará..tfólio. Aqui você encontrará..tfólio. Aqui você encontrará..tfólio. Aqui você encontrará..";
+//     let index = 0;
+
+//     function typeEffect() {
+//         if (index < text.length) {
+//             content.innerHTML += text.charAt(index);
+//             index++;
+//             setTimeout(typeEffect, 50);
+//         }
+//     }
+
+//     typeEffect();
+// });
